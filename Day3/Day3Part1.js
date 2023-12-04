f3cd8583b7df2643e9976dbb9fd68cb1ca6e7ec8
leomarker <natnael3370@gmail.com>
2023-12-04 03:21:21 +0300

    part1 solved

--- Day3/Day3Part1.js
+++ Day3/Day3Part1.js
@@ -1,46 +1,105 @@
 import fs from "fs";
 
+let numbers = [];
+let symbols = [];
+class EngineNumber {
+  constructor(value, from, to, line) {
+    this.value = value;
+    this.from = from;
+    this.to = to;
+    this.line = line;
+  }
+
+  hasSymbolInNeighbourhood(symbol) {
+    return (
+      Math.abs(this.line - symbol.line) <= 1 &&
+      this.from - 1 <= symbol.index &&
+      this.to + 1 >= symbol.index
+    );
+  }
+}
+
+class Symbol {
+  constructor(line, index, value) {
+    this.line = line;
+    this.index = index;
+    this.value = value;
+  }
+
+  numberOfNumberNeighbours() {
+    return numbers.filter((number) => number.hasSymbolInNeighbourhood(this))
+      .length;
+  }
+
+  isGear() {
+    return this.value === "*" && this.numberOfNumberNeighbours() === 2;
+  }
+
+  getNeighboursProduct() {
+    if (!this.isGear()) {
+      return 0;
+    }
+
+    const neighbours = numbers.filter((number) =>
+      number.hasSymbolInNeighbourhood(this)
+    );
+
+    return neighbours[0].value * neighbours[1].value;
+  }
+}
+
 function readInput(path, cb) {
-  let input;
+  let lines;
   fs.readFile(path, "utf-8", (err, data) => {
-    input = data.split("\n");
-
-    processInputs(input);
-
-    cb(null, input);
+    if (err) {
+      console.log(err);
+      cb(err);
+    }
+    lines = data.split("\n");
+    cb(null, lines);
   });
 }
 
-function processInputs(inputs) {
-  findPartNumber(inputs[0], inputs[1]);
+function getNumbers(lines) {
+  lines.forEach((line, i) => {
+    let j = 0;
+    while (j < line.length) {
+      if (!isNaN(line[j])) {
+        let number = line[j];
+        while (!isNaN(line[j + 1])) {
+          number += line[j + 1];
+          j++;
+        }
+        numbers.push(new EngineNumber(+number, j - number.length + 1, j, i));
+      } else if (line[j] !== ".") {
+        symbols.push(new Symbol(i, j, line[j]));
+      }
+      j++;
+    }
+  });
 }
 
-function findPartNumber(lineOne, lineTwo) {
-  let digit = false;
-  let char = false;
-  let coordinates = [];
-  let digitCoordinates = [];
+function sumNumbers(numbers) {
+  const first = numbers
+    .filter((number) =>
+      symbols.some((symbol) => number.hasSymbolInNeighbourhood(symbol))
+    )
+    .reduce((acc, number) => acc + number.value, 0);
 
-  for (let i = 0; i < lineOne.length; i++) {
-    if (lineOne[i] !== ".") {
-      if (!isNaN(lineOne[i])) {
-        if (digitCoordinates[0] === null) {
-          digitCoordinates[0] = i - 1 === -1 ? 0 : i - 1;
-        }
-      } else {
-        digitCoordinates[1] = i;
-      }
-    }
-  }
-
-  console.log(coordinates);
+  return first;
 }
 
 readInput(
   "/home/nati/learn/AdventofCode/advent_of_code_2023/Day3/input.txt",
   (err, result) => {
-    if (err) console.log(err);
+    if (err) {
+      console.log(err);
+    }
 
-    console.log(result);
+    getNumbers(result);
+
+    let sum = sumNumbers(numbers);
+
+    console.log(sum);
   }
 );
