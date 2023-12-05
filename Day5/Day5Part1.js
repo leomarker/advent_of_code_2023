7b49a14268330adc043cd746bb6cb24232cfafe0
leomarker <natnael3370@gmail.com>
2023-12-05 12:54:08 +0300

    Day five problem started

---
+++ Day5/Day5Part1.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+
+function readInput(path, cb) {
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      return cb(err);
+    }
+
+    let input = data.split("\n");
+    let map = "";
+
+    let arr = input.reduce(
+      (acc, line) => {
+        if (line === "") {
+          acc.arr.push(map);
+          map = "";
+        }
+        map = map.concat(line, "\n");
+
+        return acc;
+      },
+      { arr: [] }
+    );
+
+    cb(null, arr);
+  });
+}
+
+readInput(
+  "/home/nati/learn/AdventofCode/advent_of_code_2023/Day5/input.txt",
+  (err, input) => {
+    if (err) console.log(err);
+
+    console.log(input);
+  }
+);
