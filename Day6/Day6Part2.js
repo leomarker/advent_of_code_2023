857870aeb0e16d9c79c2e6b473083fb8a87890c8
leomarker <natnael3370@gmail.com>
2023-12-06 13:58:55 +0300

    part 2 solved

---
+++ Day6/Day6Part2.js
@@ -0,0 +1,95 @@
+import fs from "fs";
+
+let races = [];
+
+function readInput(path, cb) {
+  fs.readFile(path, "utf-8", (err, data) => {
+    if (err) {
+      return cb(err);
+    }
+
+    let input = data.split("\n");
+
+    cb(null, input);
+
+    processInput(input);
+
+    console.log(calculateMarginOfError());
+  });
+}
+
+class Race {
+  constructor(time, recordDistance) {
+    this.time = time;
+    this.recordDistance = recordDistance;
+  }
+}
+
+function processInput(input) {
+  let time;
+  let distance;
+  input.forEach((line) => {
+    if (line.startsWith("Time")) {
+      time = line
+        .split(":")[1]
+        .trim()
+        .split(" ")
+        .filter((num) => num !== "");
+    } else if (line.startsWith("Distance")) {
+      distance = line
+        .split(":")[1]
+        .trim()
+        .split(" ")
+        .filter((num) => num !== "");
+    }
+  });
+
+  let sTime = "";
+  let sDistance = "";
+
+  time.forEach((num) => (sTime = sTime.concat(num)));
+  distance.forEach((num) => (sDistance = sDistance.concat(num)));
+
+  console.log(sTime, sDistance);
+  let race;
+
+  race = new Race(sTime, sDistance);
+  races.push(race);
+}
+
+function calculateMarginOfError() {
+  let totalWaysToWin = 1;
+  races.forEach((race) => {
+    let time = race.time;
+    let distance = race.recordDistance;
+
+    let waysToWinThisRace = waysToWin(time, distance);
+
+    totalWaysToWin *= waysToWinThisRace;
+  });
+
+  return totalWaysToWin;
+}
+
+function waysToWin(time, distance) {
+  let waysToWin = 0;
+  for (let i = 14; i < time; i++) {
+    let remainingTime = time - i;
+    let speed = i;
+
+    if (remainingTime * speed > distance) {
+      ++waysToWin;
+    }
+  }
+
+  return waysToWin;
+}
+
+readInput(
+  "/home/nati/learn/AdventofCode/advent_of_code_2023/Day6/input.txt",
+  (err, input) => {
+    if (err) console.log(err);
+
+    console.log(input);
+  }
+);
